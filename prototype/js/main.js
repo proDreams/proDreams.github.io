511e149649445f3cf391ddf4adcd93edcafb854d
Ivan Ashikhmin <sushkoos@gmail.com>
2023-03-07 21:01:48 +0400

    First draft post page

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -6,3 +6,29 @@ document.getElementById('btnSwitch').addEventListener('click', () => {
         document.documentElement.setAttribute('data-bs-theme', 'dark')
     }
 })
+let copyList = document.querySelectorAll('.copy-clipboard');
+let copyArray = Array.prototype.slice.call(copyList);
+
+function tooltipUpdate(button, tooltip, title) {
+    tooltip.dispose();
+    button.setAttribute('title', title);
+    tooltip = new bootstrap.Tooltip(button);
+    tooltip.show();
+
+    return tooltip;
+}
+
+copyArray.map(function (copy) {
+    let text = copy.querySelector('code').innerText;
+    let button = copy.querySelector('button');
+    let tooltip = new bootstrap.Tooltip(button);
+
+    button.addEventListener('mouseover', function () {
+        tooltip = tooltipUpdate(button, tooltip, 'Скопировать');
+    });
+
+    button.addEventListener('click', function () {
+        window.navigator.clipboard.writeText(text);
+        tooltip = tooltipUpdate(button, tooltip, 'Готово!');
+    });
+});
